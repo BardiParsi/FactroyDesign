45cf5468ac6d1ba86de92496dc88e9daa55012bb
Masoud Farsi <156872557+BardiParsi@users.noreply.github.com>
2024-08-22 17:59:10 -0700

    Update Factory.h
    Updated factory

--- Factory.h
+++ Factory.h
@@ -16,14 +16,14 @@ import LOGGER;
 
 // General Factory template
 // This template handles creation of products with default parameters
-// This is the base class which can be inherited in the future if it's needed
+// This class will act as the base class for multiple argument calls
 template<typename... Args>
 class Factory {
 public:
     // General method to create a product; for cases with no specific type parameters
     std::tuple<ProductVariant, Args...> creatProduct(Args... args) const {
         static std::atomic<int> NoIDcounter{ 0 };
-        INFO("Creating NO ID PRODUCT/n");
+        INFO("Creating NO ID PRODUCT\n");
         NoIDcounter.fetch_add(1, std::memory_order_relaxed);
         ProductVariant product = TempProduct();
         return std::make_tuple(std::move(product), args...);
@@ -31,13 +31,13 @@ public:
 };
 // Specialization for cases with int and std::string
 // This specialization is used when creating ConcreteProductA
-// This is the base case for recursion purposes as well
+// This class will act as the recursive empty class when there is no argument left
 template<>
 class Factory<int, std::string> {
 public:
     std::tuple<ProductVariant, int, std::string> creatProduct(int id, int index, const std::string& name) const {
         static std::atomic<int> Acounter{ 0 };
-        INFO("Creating PRODUCT A/n");
+        INFO("Creating PRODUCT A\n");
         Acounter.fetch_add(1, std::memory_order_relaxed);
         ProductVariant product = ConcreteProductA(id);
         return std::make_tuple(std::move(product), index, name);
@@ -45,15 +45,15 @@ public:
 };
 // Specialization for cases with int and other types (T)
 // This specialization is used when creating ConcreteProductB
-// This class can inherit the base class if it is needed as program expands
 template<typename T>
 class Factory<int, T> {
 public:
     std::tuple<ProductVariant, int, T> creatProduct(int id, T other) const {
         static std::atomic<int> Bcounter{ 0 };
-        INFO("Creating PRODUCT B/n");
+        INFO("Creating PRODUCT B: \n");
         Bcounter.fetch_add(1, std::memory_order_relaxed);
         ProductVariant product = ConcreteProductB(id);
         return std::make_tuple(std::move(product), id, other);
     }
 };
+
